db3c99c4ef2e69f11f5a4a86120cbd35afb75536
davis scott <hao_zou@pytesgroup.com>
2023-03-07 03:06:55 +0800

    node2

--- sdk-plugin/sdk-generate/src/main/java/com/saidake/plugin/generate/data/vo/node/MethodNode.java
+++ sdk-plugin/sdk-generate/src/main/java/com/saidake/plugin/generate/data/vo/node/MethodNode.java
@@ -2,12 +2,15 @@ package com.saidake.plugin.generate.data.vo.node;
 
 import com.saidake.plugin.generate.data.vo.core.SmpTreeNode;
 
+import java.util.List;
+
 public class MethodNode extends SmpTreeNode {
+
+    private List<MethodNode> childMethodNodeList;
 
     public MethodNode(String packagePath, String filePath) {
         super(packagePath, filePath);
     }
-
     public MethodNode() {
         super();
     }
