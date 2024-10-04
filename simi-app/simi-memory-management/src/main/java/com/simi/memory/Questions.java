11a4ef0d77e5f36305f7c5f328bd511c1ab7ad14
simi <saidake@qq.com>
2024-10-05 02:13:39 +0800

    [simi-memory-management] feat: add new note headings to the storage file

--- simi-app/simi-memory-management/src/main/java/com/simi/memory/Questions.java
+++ simi-app/simi-memory-management/src/main/java/com/simi/memory/Questions.java
@@ -1,58 +1,30 @@
 package com.simi.memory;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.apache.poi.xwpf.usermodel.XWPFHeader;
-import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import com.simi.common.util.file.SimiFileUtils;
+import lombok.Cleanup;
 
 import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class Questions {
-    public static void main(String[] args) throws IOException {
-        Path path = Path.of("C:\\Users\\simi\\Desktop\\DevProject\\simi\\simi-app\\simi-memory-management\\src\\main\\resources\\storage.txt");
-        List<String> strings = Files.readAllLines(path);
-        Random random = new Random();
-        //A. get valid line
-        boolean invalidOutput=true;
-        String output=null;
-        while(invalidOutput){
-            int randomInd = random.nextInt(strings.size());
-            output = strings.get(randomInd);
-            if(output.contains(Initialization.SEPARATOR)){
-                invalidOutput=false;
-                // System.out.println("index: "+randomInd);
-            }
-        }
-        List<String> associatedHeadings=new LinkedList<>();
-        // [ 238 ] ProjectDesign = Design Patterns【设计师】[4] -> 骨人（拿起）[7] -> 香囊
+    private static final String STORAGE_FILE_PATH="/storage.txt";
+    private static final String MEMORY_LOG_FILE_PATH="C:\\Users\\simi\\Desktop\\DevProjects\\simi\\simi-app\\simi-memory-management\\src\\main\\resources\\memory-log.txt";
+    private static final List<String> PREFIX_LIST=new ArrayList<>();
+    static {
+        PREFIX_LIST.add("Config = Work Environment = awscli = Command");
+    }
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        URI storageFilePath = Objects.requireNonNull(Questions.class.getResource(STORAGE_FILE_PATH)).toURI();
+        //A. filter out valid line and obtain a random line.
+        String output = SimiFileUtils.readRandomLine(Path.of(storageFilePath), line -> PREFIX_LIST.stream().anyMatch(line::startsWith));
+        //A. filter out invalid line
         System.out.println(output);
-        //A. get prefix (e.g. ProjectDesign = Design Patterns【... -> ProjectDesign = Design Patterns ).
-        String prefix=output.replaceAll("【.*$","").replaceAll("\\[\\s[0-9]+\\s\\]","");
-        System.out.println("=============================");
-        System.out.println("pdf: "+(random.nextInt(54)+1));
-        System.out.println("=============================");
-        boolean startCheckingText=false;
-        for (String string : strings) {
-            if(!string.contains(Initialization.SEPARATOR)&&startCheckingText){
-                associatedHeadings.add(string);
-                continue;
-            }
-            if(string.contains(prefix)){
-                startCheckingText=true;
-                associatedHeadings.add(string);
-            }else{
-                startCheckingText=false;
-            }
-
-        }
-        for (String associatedHeading : associatedHeadings) {
-            System.out.println(associatedHeading);
-        }
-        FileWriter fw = new FileWriter("C:\\Users\\simi\\Desktop\\DevProject\\simi\\simi-app\\simi-memory-management\\src\\main\\resources\\memory-log.txt",true);
+        @Cleanup FileWriter fw = new FileWriter(MEMORY_LOG_FILE_PATH,true);
         fw.write(output+System.lineSeparator());
-        fw.close();
     }
 }
