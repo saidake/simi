e1b1b6c73e90b1ef5b02520a9bfe1f9e6fdbe663
simi <saidake@qq.com>
2024-10-07 23:13:39 +0800

    [simi-webflux] feat: test micrometer-tracing on spring boot 3

--- simi-test-gradle/simi-webflux/src/main/java/com/simi/webflux/controller/TestController.java
+++ simi-test-gradle/simi-webflux/src/main/java/com/simi/webflux/controller/TestController.java
@@ -2,6 +2,7 @@ package com.simi.webflux.controller;
 
 import com.simi.common.test.DataFactory;
 import com.simi.common.test.domain.TestFruit;
+import io.micrometer.tracing.annotation.NewSpan;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -10,6 +11,7 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/test")
 public class TestController {
     @GetMapping("/mono/{name}")
+    @NewSpan("create new user and group (@NewSpan)")
     public Mono<Integer> testGetMono(TestFruit fu){
         return Mono.just(fu.getAge());
     }
