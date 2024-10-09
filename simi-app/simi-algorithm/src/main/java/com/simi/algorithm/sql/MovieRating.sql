21f616cc933748a4b40f21b2a250eed4c725b1db
simi <saidake@qq.com>
2024-10-10 00:34:41 +0800

    [simi-algorithm] feat: temporary solution for MovieRating

---
+++ simi-app/simi-algorithm/src/main/java/com/simi/algorithm/sql/MovieRating.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT r1.name, r2.title
+FROM
+    (SELECT u.user_id, u.name, COUNT(mr) FROM USERS u, MovieRating mr WHERE u.user_id=mr.user_id GROUP BY u.user_id ORDER BY COUNT(mr) DESC) r1,
+    (SELECT m.movie_id, m.title, AVG(mr) FROM MOVIES m, MovieRating mr WHERE m.movie_id=mr.user_id GROUP BY u.user_id ORDER BY AVG(mr) DESC) r2
+WHERE
+# ...
