03f10db83b105c21b24038f4a65ceb1051c69a58
simi <saidake@qq.com>
2024-10-10 23:57:32 +0800

    [simi-algorithm] feat: standard solution for movieRating

--- simi-app/simi-algorithm/src/main/java/com/simi/algorithm/sql/MovieRating.sql
+++ simi-app/simi-algorithm/src/main/java/com/simi/algorithm/sql/MovieRating.sql
@@ -1,7 +1,24 @@
 # Write your MySQL query statement below
-SELECT r1.name, r2.title
-FROM
-    (SELECT u.user_id, u.name, COUNT(mr) FROM USERS u, MovieRating mr WHERE u.user_id=mr.user_id GROUP BY u.user_id ORDER BY COUNT(mr) DESC) r1,
-    (SELECT m.movie_id, m.title, AVG(mr) FROM MOVIES m, MovieRating mr WHERE m.movie_id=mr.user_id GROUP BY u.user_id ORDER BY AVG(mr) DESC) r2
-WHERE
-# ...
+#[IMP] Use LIMIT to filter out a specified number of records.
+#[IMP] Function: YEAR, MONTH
+# https://leetcode.cn/problems/movie-rating/description/
+
+(SELECT  u.name AS results
+ FROM Users u, MovieRating mr WHERE u.user_id=mr.user_id
+ GROUP BY u.user_id
+ ORDER BY COUNT(mr.rating) DESC, u.name ASC  LIMIT 1)
+UNION ALL
+(SELECT m.title AS results
+ FROM Movies  m
+          LEFT JOIN MovieRating mr
+                    ON m.movie_id=mr.movie_id AND YEAR(mr.created_at) = 2020 AND MONTH(mr.created_at) = 2
+ GROUP BY mr.movie_id
+ ORDER BY AVG(mr.rating) DESC, title ASC LIMIT 1)
+
+# Previous Solution
+# SELECT r1.name, r2.title
+# FROM
+#  (SELECT u.user_id, u.name, COUNT(mr) FROM USERS u, MovieRating mr WHERE u.user_id=mr.user_id GROUP BY u.user_id ORDER BY COUNT(mr) DESC) r1,
+#  (SELECT m.movie_id, m.title, AVG(mr) FROM MOVIES m, MovieRating mr WHERE m.movie_id=mr.user_id GROUP BY u.user_id ORDER BY AVG(mr) DESC) r2
+# WHERE
+# # ...
