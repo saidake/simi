c0985c3cc88618be286560237656bd7e6af8391c
simi <saidake@qq.com>
2024-10-14 00:23:36 +0800

    [simi-algorithm] feat: passed solution for "Friday Purchases II"

--- simi-app/simi-algorithm/src/main/java/com/simi/algorithm/sql/FridayPurchasesII.sql
+++ simi-app/simi-algorithm/src/main/java/com/simi/algorithm/sql/FridayPurchasesII.sql
@@ -6,6 +6,7 @@
 # [ER] List all results for those Fridays regardless of whether the record exits.
 # [IM] Functions: DATE_ADD,  DAYOFWEEK, CEIL, COALESCE
 # [IM] Operators: WITH RECURSIVE ... AS, INTERVAL ... DAY
+# [IM} Because the GROUP BY clause is processed before the SELECT clause, the alias defined in the SELECT clause isn't yet available for grouping.
 # ---------------------------------------------------- Updated Solution
 # Define a common table expression.
 # DAYOFWEEK: Sunday is 1 and Saturday is 7.
@@ -15,13 +16,18 @@ WITH RECURSIVE cte AS(
     SELECT DATE_ADD(friday, INTERVAL 7 DAY) AS friday FROM cte
     WHERE  MONTH(DATE_ADD(friday, INTERVAL 7 DAY))=11
 )
-SELECT (DAY(purchase_date)-3)/7 +1 AS week_of_month, purchase_date, SUM(amount_spend) AS total_amount
+#     | friday     |
+#     | ---------- |
+#     | 2023-11-03 |
+#     | 2023-11-10 |
+#     | 2023-11-17 |
+#     | 2023-11-24 |
+SELECT CEIL(DAY(cte.friday)/7) AS week_of_month, cte.friday AS purchase_date, IFNULL(SUM(amount_spend), 0) AS total_amount
 FROM cte
-         LEFT JOIN Purchases p
-                   ON cte.friday = p.purchase_date
-WHERE MONTH(purchase_date)=11 AND YEAR(purchase_date)=2023 AND DAY(purchase_date) in (3,10,17,24)
-GROUP BY purchase_date;
-
+LEFT JOIN Purchases p
+ON cte.friday = p.purchase_date
+#GROUP BY purchase_date;
+GROUP BY cte.friday;
 # ---------------------------------------------------- Previous Solution
 SELECT (DAY(purchase_date)-3)/7 +1 AS week_of_month, purchase_date, SUM(amount_spend) AS total_amount
 FROM Purchases
