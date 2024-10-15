8d55b147eeff738ec75cb04d7690e558e79371c3
simi <saidake@qq.com>
2024-10-16 00:12:13 +0800

    [simi-algorithm] feat: passed solution for "The Number of Users That Are Eligible for Discount"

--- simi-app/simi-algorithm/src/main/java/com/simi/algorithm/sql/TheNumberOfUsersThatAreEligibleForDiscount.sql
+++ simi-app/simi-algorithm/src/main/java/com/simi/algorithm/sql/TheNumberOfUsersThatAreEligibleForDiscount.sql
@@ -12,3 +12,12 @@ BEGIN
         HAVING SUM(amount) > minAmount
     );
 END
+# ---------------------------------------------------- Passed Solution
+CREATE FUNCTION getUserIDs(startDate DATE, endDate DATE, minAmount INT) RETURNS INT
+BEGIN
+        DECLARE userCount INT;
+        SELECT COUNT(DISTINCT user_id) INTO userCount
+        FROM Purchases
+        WHERE time_stamp BETWEEN startDate AND endDate AND amount> minAmount;
+        RETURN COALESCE(userCount, 0);
+END
