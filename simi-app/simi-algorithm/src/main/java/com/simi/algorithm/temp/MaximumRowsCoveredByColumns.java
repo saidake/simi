47b309e8ebebfb85c37f0e6850f7b417fc55fcdf
Craig Brown <saidake@qq.com>
2024-10-22 23:58:23 +0800

    [simi-algorithm] feat: temporary logic2 for the "Maximum Rows Covered by Columns" problem

--- simi-app/simi-algorithm/src/main/java/com/simi/algorithm/temp/MaximumRowsCoveredByColumns.java
+++ simi-app/simi-algorithm/src/main/java/com/simi/algorithm/temp/MaximumRowsCoveredByColumns.java
@@ -1,5 +1,7 @@
 package com.simi.algorithm.temp;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Arrays;
 import java.util.OptionalInt;
 
@@ -31,18 +33,28 @@ public class MaximumRowsCoveredByColumns {
         //A. Get the number of '1' of each column.
         int rLen = matrix.length; // 1 <= m, n <= 12;
         int cLen = matrix[0].length;
-        int[] cNumList = new int[cLen];
-        int[] selectedC = new int[numSelect];
+        int[] decimalArr=new int[rLen];
         for (int i = 0; i < rLen; i++) {
+            int dec=0;
             for (int j = 0; j < cLen; j++) {
-                int item = matrix[i][j];
-                if (item == 1) cNumList[j] += 1;
+                //B. the decimal number start from the left to right of the array (e.g. [0,1,1] -> 110 -> 6).
+                if(matrix[i][j]==1)dec+=1<<j;
+            }
+            decimalArr[i]=dec;
+        }
+        //A. Remove a specified number of columns and check whether each row is empty according to the mask of each row.
+        int maxNum=0;
+        for (int i = 0; i < cLen; i++) {
+            for (int j = i+1; j < cLen; j++) {
+                int mask=1<<i+1<<j;
+                int curNum=0;
+                for (int dec : decimalArr) {
+                    if((dec&mask)==0)curNum++;
+                }
+                maxNum=Math.max(maxNum,curNum);
             }
         }
-
-        return 0;
+        return maxNum;
     }
 
-    public void dfs(int[][] matrix, int numSelect) {
-    }
 }
---
+++ simi-app/simi-algorithm/src/main/java/com/simi/algorithm/temp/TempTest.java
@@ -0,0 +1,9 @@
+package com.simi.algorithm.temp;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TempTest {
+    public static void main(String[] args) {
+    }
+}
