ce6b43c7fd53c5e36c8c8801781f632391df5d40
Craig Brown <saidake@qq.com>
2024-10-29 23:17:59 +0800

    [simi-algorithm] feat: passed solution for the "Maximum Average Subtree" problem

--- simi-app/simi-algorithm/src/main/java/com/simi/algorithm/temp/MaxSumOfRectangleNoLargerThanK.java
+++ simi-app/simi-algorithm/src/main/java/com/simi/algorithm/temp/MaxSumOfRectangleNoLargerThanK.java
@@ -12,32 +12,9 @@ import java.util.*;
  *     1 <= m, n <= 100
  *     -100 <= matrix[i][j] <= 100
  *     -105 <= k <= 105
- * ---------------------------------------------------------- Analysis
+ * ---------------------------------------------------------- Examples
  * max sum = k
  * m x n =  9 x 6
- * 1   0   1   9  -1   2  -3  -4   3
- * 3  -2   3   8   9  -7   7  -6   2
- * 4 [ 3   8   9  -1]  2   3   1   3
- * 2 [-2   3   8   8] -7  -6   2  -4
- * 5 [ 0   5   9  -1]  4  -5   1   1
- * 3  -9   1  -2   3  -4   9  -6  -8
- * First assume that the target rectangle is the following rectangle:
- * max sum = 19 + 17 + 13 = 49
- *  3  8  9 -1
- * -2  3  8  8
- *  0  5  9 -1
- * Consideration:
- *     The size of the rectangle cannot be determined.
- *     The location of the rectangle cannot be determined.
- * Optional Conditions:
- *     Mark all negative numbers. (invalid)
- *     First calculate the sum of each row and the sum of each column.
- * Initialization:
- *     Start from the first element
- * Core Logic:
- *     Position the rectangle based on the upper left and lower right corner points.
- *     The diffusion algorithm only need half points as start points in the matrix.
- * ---------------------------------------------------------- Traversal
  * 1   0   1   9  -1   2  -3  -4   3
  * 3  -2   3   8   9  -7   7  -6   2
  * 4   3   8   9  -1   2   3   1   3
@@ -65,11 +42,10 @@ public class MaxSumOfRectangleNoLargerThanK {
             for (int cur = start; cur < cLen; cur++) {
                 //B. Update the row sums for the current column range [left, right].
                 // 1   0   1   9  -1   2  -3  -4   3        rowSums[0] = 1  -> 1  -> 2
-                // ↓   ↓   ↓
+                // ↓   ↓   ↓   ...
                 // 3  -2   3                                rowSums[1] = 3  -> 1  -> 4
                 // ↓   ↓   ↓
                 // 4   3   8                                rowSums[2] = 4  -> 7  -> 15
-                // ...
                 for (int i = 0; i < rLen; i++) {
                     rowSums[i] += matrix[i][cur];
                 }
---
+++ simi-app/simi-algorithm/src/main/java/com/simi/algorithm/temp/MaximumAverageSubtree.java
@@ -0,0 +1,72 @@
+package com.simi.algorithm.temp;
+
+import com.simi.common.test.domain.TreeNode;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * https://leetcode.cn/problems/maximum-average-subtree/description/
+ * Maximum Average Subtree
+ * <pre>
+ * Constraints:
+ *      The number of nodes in the tree is in the range [1, 104].
+ *      0 <= Node.val <= 105
+ * </pre>
+ */
+@Slf4j
+public class MaximumAverageSubtree {
+    private double maxAvgSum=0;
+    private class BackTrackObj {
+        public int sum=0;
+        public int nodeNum=0;
+
+        public BackTrackObj(int sum, int nodeNum){
+            this.sum=sum;
+            this.nodeNum=nodeNum;
+        }
+    }
+    public double maximumAverageSubtree(TreeNode root) {
+        postorder(root);
+        return this.maxAvgSum;
+    }
+
+    /**
+     * Traverse binary tree in postorder.
+     * Node value range: 0 <= Node.val <= 105
+     *                            2
+     *                 6                      3
+     *            -          5          12           7
+     *                    -    -     0     4        -    10
+     *                             - -    11   -        -   9
+     *                                   - 1               -  8
+     * @param root  Root node
+     */
+    private BackTrackObj postorder(TreeNode root){
+        //A. Creating the BackTrackObj to store return values is necessary because there is more than one return value.
+        if(root!=null){
+            BackTrackObj lo=postorder(root.left);
+            BackTrackObj ro=postorder(root.right);
+            int nodeNum=0;
+            int curSum=0;
+            if(root.left!=null&&root.right!=null){
+                nodeNum=lo.nodeNum + ro.nodeNum + 1;
+                curSum=lo.sum+ro.sum+root.val;
+            }
+            else if(root.left!=null){
+                nodeNum=lo.nodeNum  + 1;
+                curSum=lo.sum+root.val;
+            }
+            else if(root.right!=null) {
+                nodeNum=ro.nodeNum  + 1;
+                curSum=ro.sum+root.val;
+            }
+            else {
+                nodeNum=1;
+                curSum=root.val;
+            }
+            double curAvg=(double)curSum/nodeNum;
+            this.maxAvgSum=Math.max(maxAvgSum,curAvg);
+            return new BackTrackObj(curSum, nodeNum);
+        }
+        return null;
+    }
+}
