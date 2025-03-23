de2a5dbec19762ea8108fc7815ab59ae068b1009
Craig Brown <saidake@qq.com>
2025-03-23 19:10:02 +0800

    [simi-review-tool] feat: Add a match pattern "NUMBER: "

--- simi-app/simi-review-tool/src/main/java/com/simi/review/ReviewToolApp.java
+++ simi-app/simi-review-tool/src/main/java/com/simi/review/ReviewToolApp.java
@@ -30,23 +30,21 @@ import java.util.regex.Pattern;
 @Slf4j
 public class ReviewToolApp {
     private static final Path KEY_POINTS =Path.of("C:\\Users\\simi\\Desktop\\DevProjects\\simi-docs\\temp\\KeyPoints.md");
-    private static final Pattern FILE_PATH_PATTERN =Pattern.compile("^FILE: '(.*?)'$");
-    private static final Pattern MATCH_PATTERN =Pattern.compile("^MATCH_PATTERN: '(.*?)'$");
-    private static final Pattern REMOVAL_PATTERN =Pattern.compile("^REMOVAL_PATTERN: '(.*?)'$");
+    private static final Pattern FILE_PATH_PATTERN =Pattern.compile("^FILE: '(.*?)'\\s*$");
+    private static final Pattern MATCH_PATTERN =Pattern.compile("^MATCH_PATTERN: '(.*?)'\\s*$");
+    private static final Pattern REMOVAL_PATTERN =Pattern.compile("^REMOVAL_PATTERN: '(.*?)'\\s*$");
+    private static final Pattern NUMBER_PATTERN =Pattern.compile("^NUMBER: ([1-9]+)\\s*$");
 
     private static final Path REVIEW_LOG_FILE_PATH =Path.of("C:\\Users\\simi\\Desktop\\DevProjects\\simi-docs\\temp\\ReviewLogs.md");
 
-
     private static final DateTimeFormatter dateFormater= DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    private static final Pattern PRIORITY_PATTERN = Pattern.compile("\\[(\\d+)]$");
 
     public static void main(String[] args) throws IOException {
         System.out.println("\\*");
         @Cleanup FileWriter fw = new FileWriter(REVIEW_LOG_FILE_PATH.toString(),true);
-        readMatchPatterns().forEach(System.out::println);
-        // Select a random line
-        //writeReviewLog(fw, japWord);
-        //fw.write(System.lineSeparator());
+        Stack<String> lines=readMatchPatterns();
+        System.out.println("-------------------------------------------");
+        lines.forEach(System.out::println);
     }
 
     private static Stack<String> readMatchPatterns() throws IOException {
@@ -55,56 +53,66 @@ public class ReviewToolApp {
         @Cleanup BufferedReader reader = new BufferedReader(new FileReader(KEY_POINTS.toString()));
         // Read random lines from external files
         String line;
+        int skipLineNum=0;
+
         Path filePath=null;
-        int lineNum=0;
+        int randomLineNum=1;
+        String currentMatchPattern=null;
+        String currentRemovalPattern=null;
         while ((line = reader.readLine()) != null) {
-            lineNum++;
+            skipLineNum++;
             if(line.isEmpty())continue;
             Matcher filePathMatcher=FILE_PATH_PATTERN.matcher(line);
             Matcher matchMatcher=MATCH_PATTERN.matcher(line);
             Matcher removalMatcher=REMOVAL_PATTERN.matcher(line);
+            Matcher numberMatcher=NUMBER_PATTERN.matcher(line);
             if(filePathMatcher.find()){
                 // Add a file name prefix for the previous matched line
-                if(!res.isEmpty()){
-                    String curHandledStr=res.pop();
-                    res.push(getFileNamePrefix(filePath.getFileName().toString())+curHandledStr);
+                if(filePath!=null){
+                    Assert.notNull(currentMatchPattern);
+                    pushRandomLine(randomLineNum, currentMatchPattern, filePath, currentRemovalPattern, res);
+                    currentMatchPattern=null;
+                    currentRemovalPattern=null;
+                    randomLineNum=1;
                 }
                 String filePathStr=filePathMatcher.group(1);
                 filePath=Path.of(filePathStr);
-                log.info("--------------------------------------");
-                log.info("filePath: {}",filePath);
-
             } else if(filePath!=null&&matchMatcher.find()){
-                String currentMatchPattern=matchMatcher.group(1);
-                log.info("currentMatchPattern: {}", currentMatchPattern);
-                String currentRandomLine=SimiFileUtils.readRandomLine(filePath,item->item.matches(currentMatchPattern));
-                log.info("currentRandomLine: {}", currentRandomLine);
-                res.push(currentRandomLine);
-            } else if(filePath!=null&&removalMatcher.find()){
-                String currentRemovalPattern=removalMatcher.group(1);
-                log.info("currentRemovalPattern: '{}'", currentRemovalPattern);
-                log.info("res.peek(): {}", res.peek());
+                currentMatchPattern=matchMatcher.group(1);
+            } else if(filePath!=null&&removalMatcher.find()) {
+                currentRemovalPattern = removalMatcher.group(1);
+            } else if (filePath!=null&&numberMatcher.find()){
+                String currentNumber=numberMatcher.group(1);
                 Assert.notNull(res.peek());
-                String temp=res.pop().replaceAll(currentRemovalPattern,"");
-                log.info("temp: {}", temp);
-                res.push(temp);
+                randomLineNum=Integer.valueOf(currentNumber);
             }else{
-                if(lineNum>0)lineNum--;
+                if(skipLineNum>0)skipLineNum--;
                 // Add a file name prefix for the previous matched line
-                if(!res.isEmpty()){
-                    String curHandledStr=res.pop();
-                    res.push(getFileNamePrefix(filePath.getFileName().toString())+curHandledStr);
+                if(filePath!=null){
+                    Assert.notNull(currentMatchPattern);
+                    pushRandomLine(randomLineNum, currentMatchPattern, filePath, currentRemovalPattern, res);
                 }
                 break;
             }
         }
         // Read a random line form the Key Point file
-        log.info("lineNum: {}",lineNum);
-        List<String> collect = Files.lines(KEY_POINTS).skip(lineNum).filter(keyPointLine->!keyPointLine.isEmpty()).toList();
+        log.info("lineNum: {}",skipLineNum);
+        List<String> collect = Files.lines(KEY_POINTS).skip(skipLineNum).filter(keyPointLine->!keyPointLine.isEmpty()).toList();
         Assert.notEmpty(collect);
         String curLine=collect.get(ThreadLocalRandom.current().nextInt(collect.size()));
         res.push(getFileNamePrefix(KEY_POINTS.getFileName().toString())+curLine);
         return res;
+    }
+
+    private static void pushRandomLine(int randomLineNum, String currentMatchPattern, Path filePath, String currentRemovalPattern, Stack<String> res) throws IOException {
+        for(; randomLineNum >0; randomLineNum--){
+            String finalCurrentMatchPattern = currentMatchPattern;
+            String result=SimiFileUtils.readRandomLine(filePath, item->item.matches(finalCurrentMatchPattern));
+            if(currentRemovalPattern!=null) result = result.replaceAll(currentRemovalPattern, "");
+            log.info("result: {}", result);
+            result=getFileNamePrefix(filePath.getFileName().toString())+result;
+            res.push(result);
+        }
     }
 
     /**
@@ -115,60 +123,5 @@ public class ReviewToolApp {
      */
     private static String getFileNamePrefix(String fileName){
         return fileName.replaceAll(".md","")+": ";
-    }
-
-    /**
-     * Write the output string into the {@code REVIEW_LOG_FILE_PATH} file.
-     *
-     * @param fw    File Writer
-     * @param output    Output String
-     * @throws IOException IO Exception
-     */
-    private static void writeReviewLog(FileWriter fw, String output) throws IOException {
-        // fw.write(LocalDateTime.now().format(dateFormater) + " - "+ output);
-        fw.write(output);
-        fw.write(System.lineSeparator());
-    }
-
-    /**
-     * Filter out a line starting with specific prefixes.
-     *
-     * @param prefixes Prefixes
-     * @return A line
-     * @throws IOException IO exception
-     */
-    private static String generateARandomLineStartingWith(Path path,String ... prefixes) throws IOException {
-        return SimiFileUtils.readRandomWeightedLine(
-                path,
-                line -> !line.isEmpty() && Arrays.stream(prefixes).anyMatch(line::startsWith),
-                line-> {
-                    Matcher matcher=PRIORITY_PATTERN.matcher(line);
-                    if(matcher.find()){
-                        return Float.parseFloat(matcher.group(1));
-                    }
-                    return 1.0F;
-                }
-        );
-    }
-
-
-    /**
-     * Filter out a random line from the {@code REVIEW_PLAN_PATH} path.
-     *
-     * @return A line
-     * @throws IOException IO exception
-     */
-    private static String generateARandomLine(Path path) throws IOException {
-        return SimiFileUtils.readRandomWeightedLine(
-                path,
-                line -> !line.isEmpty(),
-                line-> {
-                    Matcher matcher=PRIORITY_PATTERN.matcher(line);
-                    if(matcher.find()){
-                        return Float.parseFloat(matcher.group(1));
-                    }
-                    return 1.0F;
-                }
-        );
     }
 }
